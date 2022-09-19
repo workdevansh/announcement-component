9589b8fa481c14181cc5dd2539105129bf0b3079
Devansh Tiwari <devanshtiwari365@gmail.com>
2022-09-19 20:50:07 +0530

    Add announcement components

---
+++ components/Announcement/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import styles from './announcement.module.css'
+
+const Announcement = () => {
+  return (
+    <div className={styles.container}>🎉 Fuel aggregator Nawgati raises funding from MapMyIndia
+    </div>
+  )
+}
+
+export default Announcement
